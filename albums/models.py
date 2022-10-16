ec99a109e3b75548da51da8c0cfb0c6f01fa6ad7
Maryam El-Fdaly <maryam_elfdaly@hotmail.com>
2022-10-16 03:26:59 +0200

    all except 6

--- albums/models.py
+++ albums/models.py
@@ -1,9 +1,10 @@
+from email.policy import default
 from django.db import models
 from artists.models import Artist
 import datetime
 from django.utils import timezone
 
-# Create your models here.
+# Album Model
 
 class Album(models.Model):
     artist=models.ForeignKey(Artist,on_delete=models.CASCADE)
@@ -11,6 +12,8 @@ class Album(models.Model):
     creation_datetime = models.DateTimeField()
     release_datetime=models.DateTimeField(blank=False)
     cost=models.DecimalField(max_digits=10,decimal_places=2)
+    # (1.Add a boolean field to the album model )
+    is_approved=models.BooleanField(default=False)
     def __str__(self) :
         return self.name
 
